5771e39a91a1b937140902c49e4cb90d05822e70
Ilya Rusanen <ilya@rusanen.co.uk>
2019-07-04 17:38:50 +0300

    fix (PLAN-18): Schedule template improvements

--- plan/views/index.py
+++ plan/views/index.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Tuple, Generator
 
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
@@ -7,13 +8,11 @@ from django.shortcuts import render
 from main.models import Post, Idea, Issue
 
 
-def get_schedule_starttime():
+def get_schedule_starttime() -> Tuple[datetime.datetime, datetime.datetime]:
     """Get time range for schedule
 
-    Counts two dates:
-    * beginning of this week
-    * end of next week
-    :return:
+    Counts two dates: * beginning of this week
+    * end of next week :return:
     """
     today = datetime.datetime.today().replace(hour=0, minute=0, second=0)
     this_week_begin = today - datetime.timedelta(days=today.weekday())
@@ -22,9 +21,28 @@ def get_schedule_starttime():
     return this_week_begin, next_week_end
 
 
-def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + datetime.timedelta(n)
+def daterange(start_date: datetime.datetime, end_date: datetime.datetime,
+              skip_weekend: bool = True) -> Generator:
+    """Yield every day date between two dates.
+
+    Pre-fixes provided dates to include full day cycle.
+    @generator
+    """
+    weekend_days = (5, 6)
+
+    # Fix daterange to include full day cycle to prevent comparison failures
+    start_date = start_date.replace(hour=0, minute=0, second=0)
+    end_date = end_date.replace(hour=23, minute=59, second=59)
+    start_day_weekday = start_date.weekday()
+
+    for days in range(int((end_date - start_date).days)):
+        if skip_weekend is False:
+            yield start_date + datetime.timedelta(days)
+            continue
+
+        next_date_weekday = (start_day_weekday + days) % 7
+        if next_date_weekday not in weekend_days:
+            yield start_date + datetime.timedelta(days)
 
 
 # Create your views here.
@@ -35,12 +53,14 @@ def index(request):
         Q(stage__assignee__isnull=True, editor=request.user, )
     ).exclude(stage__slug__in=['vault', 'published'])
 
-    need_to_vote = Idea.objects.filter(approved__isnull=True).exclude(votes__user=request.user)
+    need_to_vote = Idea.objects.filter(approved__isnull=True) \
+        .exclude(votes__user=request.user)
 
-    # TODO: rewrite as negative query
     opened_issues = Issue.objects.filter(
-        posts__stage__slug__in=['waiting', 'proofreading_editor', 'precheck', 'spellcheck', 'markup',
-                                'proofreading_spell', 'proofreading_chief_dpt', 'proofreading_chief', 'publishing']) \
+        posts__stage__slug__in=['waiting', 'proofreading_editor', 'precheck',
+                                'spellcheck', 'markup', 'proofreading_spell',
+                                'proofreading_chief_dpt', 'proofreading_chief',
+                                'publishing']) \
         .distinct()
 
     # Count schedule
@@ -50,12 +70,10 @@ def index(request):
                                           published_at__lte=time_end)
 
     for day in daterange(time_start, time_end):
-        # HACK: used to guaranteed suite day interval to today determination
-        day_ = day.replace(hour=6, minute=6, second=6)
-        schedule[day_] = list(filter(
+        schedule[day] = list(filter(
             lambda p: day.astimezone() <= p.published_at <= day.astimezone() + datetime.timedelta(days=1),
             scheduled_posts))
-    print(schedule)
+
     return render(request, 'plan/index/index.html', {
         'self_posts': self_posts,
         'need_to_vote': need_to_vote,
