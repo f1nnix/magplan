5283382e9bf82bfd602d56821ab25fec36c47f49
Ilya Rusanen <ilya@rusanen.co.uk>
2019-10-17 15:30:34 +0300

    feat (PLAN-35): Add imaga renderer tests

--- xmd/tests/test_renderer.py
+++ xmd/tests/test_renderer.py
@@ -3,41 +3,44 @@ Every block element test will be automatically
 wrapped inside `<p></p>\n`. Thats why every block
 test should include this wrapper tag.
 """
+from io import BytesIO
+from unittest import TestCase
 from unittest.mock import patch
 
-from xmd import render_md
+import pytest
+from django.core.files import File
+from django_dynamic_fixture import G
+
+from main.models import Attachment
+from xmd.renderer import XMDRenderer
 
 
-class TestImage:
-    expected_html = (
-        '<p>'
-        '<figure>'
-        '<img src="dummy.jpg" alt="foo" /><figcaption>foo</figcaption>'
-        '</figure>'
-        '</p>\n'
-    )
+@pytest.mark.django_db
+class TestImage(TestCase):
+    def setUp(self):
+        file1 = File(
+            name='file1.jpg',
+            file=BytesIO(b'abcdef')
+        )
+        attachment1 = G(Attachment, original_filename='user_friendly_filename1.jpg', file=file1)
+
+        self.renderer = XMDRenderer(
+            attachments=[attachment1]
+        )
+
+        self.expected_html = (
+            '<figure>'
+            '<img src="dummy.jpg" alt="foo" /><figcaption>foo</figcaption>'
+            '</figure>'
+        )
 
     @patch('xmd.renderer.get_attachment_original_filename')
-    def test_render(self, mock_get_attachment_original_filename):
+    def test_render_image(self, mock_get_attachment_original_filename):
         mock_get_attachment_original_filename.return_value = 'dummy.jpg'
 
-        md = '![foo](bar)'
-        assert render_md(md) == self.expected_html
+        html = self.renderer.image('bar', 'foo', 'foo')
 
-    @patch('xmd.renderer.get_attachment_original_filename')
-    def test_whitespace_end(self, mock_get_attachment_original_filename):
-        mock_get_attachment_original_filename.return_value = 'dummy.jpg'
-
-        md = '![foo](bar)               '
-        assert render_md(md) == self.expected_html
-
-
-class TestPanel:
-    def test_panel_whitespace_begin(self):
-        ...
-
-    def test_panel_whitespace_end(self):
-        ...
-
-    def test_panel_whitespace_oneliner(self):
-        ...
+        mock_get_attachment_original_filename.assert_called_with(
+            'bar', self.renderer.attachments
+        )
+        assert html == self.expected_html
