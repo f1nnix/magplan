4cb1b55746fa3f4704b8f2d3ea406c1dae5e70e4
Ilya Rusanen <ilya@rusanen.co.uk>
2020-12-20 15:46:04 +0300

    MP-3: remove obsolete email backends

--- src/magplan/email_backends.py
+++ src/magplan/email_backends.py
@@ -158,31 +158,3 @@ class DefaultEmailBackend(EmailBackend):
                 "one of those settings to True.")
         self.connection = None
         self._lock = threading.RLock()
-
-
-class FinanceEmailBackend(EmailBackend):
-    def __init__(self, host=None, port=None, username=None, password=None,
-                 use_tls=None, fail_silently=True, use_ssl=None, timeout=None,
-                 ssl_keyfile=None, ssl_certfile=None,
-                 **kwargs):
-        super().__init__(fail_silently=fail_silently)
-
-        self.host = config.FINANCE_EMAIL_HOST
-        self.port = config.FINANCE_EMAIL_PORT
-        self.username = config.FINANCE_EMAIL_USER
-        self.password = config.FINANCE_EMAIL_PASSWORD
-        self.use_tls = config.FINANCE_EMAIL_USE_TLS
-
-        self.use_ssl = settings.EMAIL_USE_SSL if use_ssl is None else use_ssl
-        self.timeout = settings.EMAIL_TIMEOUT if timeout is None else timeout
-        self.ssl_keyfile = settings.EMAIL_SSL_KEYFILE if ssl_keyfile is None else ssl_keyfile
-        self.ssl_certfile = settings.EMAIL_SSL_CERTFILE if ssl_certfile is None else ssl_certfile
-
-        self.from_email = config.FINANCE_EMAIL_FROM
-
-        if self.use_ssl and self.use_tls:
-            raise ValueError(
-                "EMAIL_USE_TLS/EMAIL_USE_SSL are mutually exclusive, so only set "
-                "one of those settings to True.")
-        self.connection = None
-        self._lock = threading.RLock()
