225cbb3ab4e2249746652d067ce44f31454410ec
Ilya Rusanen <ilya@rusanen.co.uk>
2021-03-17 20:34:42 +0300

    MP-62: basic sites integration

--- src/magplan/context_processors.py
+++ src/magplan/context_processors.py
@@ -1,6 +1,8 @@
-from magplan.models import Issue
 import os
-# from django.contrib.sites.models import Site
+
+from django.contrib.sites.models import Site
+
+from magplan.models import Issue
 
 
 def inject_last_issues(request):
@@ -11,10 +13,14 @@ def inject_last_issues(request):
 
 
 def inject_app_url(request):
-    # def current_site_failback():
-    #     current_site = Site.objects.get_current()
-    #     return current_site.domain
-
     return {
         'APP_URL': os.environ.get('APP_URL', None),
+    }
+
+
+def inject_sites(request):
+    current_site: Site = Site.objects.get_current(request=request)
+    return {
+        'current_site': current_site,
+        'sites': Site.objects.exclude(id=current_site.id),
     }
--- src/magplan/utils.py
+++ src/magplan/utils.py
@@ -1,5 +1,9 @@
 from typing import Any, Callable
 
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.http import HttpRequest
+
 
 def safe_cast(value: Any, to: Callable, on_error: Any = None) -> Any:
     """"Safe casts value to type with provided casting constructor.
@@ -17,3 +21,20 @@ def safe_cast(value: Any, to: Callable, on_error: Any = None) -> Any:
         return on_error
 
     return casted
+
+
+def get_current_site(request: HttpRequest, safe: bool = True) -> Site:
+    """
+    Returns selected on plan site from user settings
+    """
+    current_site_id = request.user.preferences['magplan__current_site']  # noqa
+    if current_site_id:
+        try:
+            Site.objects.get(id=current_site_id)
+        except Site.DoesNotExist:
+            pass  # FIXME
+
+    DEFAULT_SITE_ID: int = settings.SITE_ID
+
+    # Should exists, don't handle exceptions
+    return Site.objects.get(id=DEFAULT_SITE_ID)
