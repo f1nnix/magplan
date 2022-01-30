3c1f528a7d003161eb64838e6d2e0c747758f408
Ilya Rusanen <ilya@rusanen.co.uk>
2022-01-30 15:50:52 +0000

    Bump version to 2.0.28

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ extras_requirements = {
 
 setup(
     name="magplan",
-    version="2.0.27",
+    version="2.0.28",
     author="Ilya Rusanen",
     author_email="ilya@rusanen.co.uk",
     url="",
