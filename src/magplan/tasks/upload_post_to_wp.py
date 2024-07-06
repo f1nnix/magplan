b7dac54f441cfad5693cd625bc26c51f511a5656
Ilya Rusanen <ilya@rusanen.co.uk>
2024-07-06 16:10:15 +0300

    MP-92: autoretry WP upload tasks with Celery

--- src/magplan/tasks/upload_post_to_wp.py
+++ src/magplan/tasks/upload_post_to_wp.py
@@ -1,19 +1,21 @@
-# Create your tasks here
-from __future__ import absolute_import, unicode_literals
-
 import logging
 
 from celery import shared_task
 
 from magplan.models import Post
 from magplan.integrations.posts import Lock
+from pymysql.err import OperationalError
 
 
 logger = logging.getLogger()
 
 
-@shared_task
-def upload_post_to_wp(post_id: int) -> None:
+@shared_task(
+    autoretry_for=(OperationalError,),
+    retry_kwargs={'max_retries': 5, 'countdown': 10},  # Retry up to 3 times with a 60 seconds delay between retries
+    retry_backoff=False  # Optional: exponential backoff
+)
+def upload_post_to_wp(self, post_id: int) -> None:
     logger.info("Starting task upload_post_to_wp...")
 
     post = Post.objects.filter(id=post_id).first()
@@ -21,5 +23,5 @@ def upload_post_to_wp(post_id: int) -> None:
         return
 
     with Lock(post):
-        logger.info("Lock for post %s acquired" % post.id)
+        logger.info("Lock for post %s acquired", post.id)
         post.upload()
